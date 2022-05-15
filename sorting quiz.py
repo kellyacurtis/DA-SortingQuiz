4c9d6de8f4b29fd8379eccc6ecff3ef30434c574
kellyacurtis <100333008+kellyacurtis@users.noreply.github.com>
2022-05-15 12:07:05 -0400

    Update sorting quiz.py

--- sorting quiz.py
+++ sorting quiz.py
@@ -8,17 +8,19 @@ hogwarts_house = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
 
 
 sorting_name = f"Hello, {name}! Welcome to the Sorting Hat..."
-
 print(sorting_name)
+
 print("Answer each question to find your Hogwarts House!")
 
-
+#reads file consisting of sorting questions
 sorting_hat_file = open("sortingquestions.txt", "r")
 line = sorting_hat_file.readline()
 sortingquestion = line.split(",")
 
-HogwartsHouse = [] # list of each house answer 
+# accounts for every corresonding answer
+HogwartsHouse = []  
 
+#while quiz is not finished, loop through each question to account for answer
 while sortingquestion[0] != "END":
     print(sortingquestion[0])
     
@@ -39,16 +41,17 @@ while sortingquestion[0] != "END":
 
 print(HogwartsHouse)
 
-# tallies the number of houses chosen in questions
+# counts the number of houses chosen in questions
 Gryffindor = HogwartsHouse.count("G")
 Slytherin = HogwartsHouse.count("S")
 Hufflepuff = HogwartsHouse.count("H")
 Ravenclaw = HogwartsHouse.count("R")
 
+#lists each 
 list_sorted_houses = sorted([(Gryffindor, "G"), (Slytherin, "S"), (Hufflepuff, "H"), (Ravenclaw, "R")], key = None, reverse = True)
 
 print(list_sorted_houses[0])
-
+#announces sorted house bases on count
 if "G" in list_sorted_houses[0]:
     print(f'Congratulations, you have been sorted into Gryffindor!')
 
